032831bebe7e4160e72a8f28d856a96ec86390b6
Tomasz Morawiec <moraviec@gmail.com>
2019-01-14 14:07:29 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstProject';
+
+
+  comments: Array<Object>;
+
+  constructor() {
+    this.comments = [
+  {
+    name: 'Tomek',
+    comment: 'lorem ipsum',
+    hide: true
+
+  },
+  {
+    name: 'Jan',
+    comment: 'lorem ifsdafpsum asdd 2',
+    hide: true
+  },
+  {
+    name: 'Stefan',
+    comment: 'lasd ifsdafpsum asdd 2',
+    hide: true
+  },
+  {
+    name: 'Janusz',
+    comment: 'Elsos lorem ifsdafpsum asdd 2',
+    hide: false
+  },
+  {
+    name: 'Januszek',
+    comment: 'Elsos lorem ifsdafpsum asdd 2',
+    hide: false
+  }];
+}
+
+toggle(comment) {
+  comment.hide = !comment.hide;
+};
+
+getCounterColor(comments) {
+  if (comments.length < 2) {
+    return 'red';
+  } 
+  if (comments.length > 1 && comments.length < 5) {
+    return 'orange';
+  } 
+  else {
+    return 'green';
+  }
+  
+};
+
+
+
+  title = 'Aplikacja do komentowania';
+  
+
 }
